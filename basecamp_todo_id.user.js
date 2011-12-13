11d83d01f86bb67a14ccfe0c05615d822c39c4c0
Philipp Waldhauer <ich@philippwaldhauer.de>
2011-12-13 18:22:49 +0100

    first commit

---
+++ basecamp_todo_id.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name           Basecamp Todo ID
+// @namespace      de.knuspermagier
+// @description    Shows the todo id in basecamp
+// @include        https://quotefm.basecamphq.com/*
+// ==/UserScript==
+
+var items = document.getElementsByTagName("div");
+
+for(var i = 0; i < items.length; i++) {
+	var item = items[i];
+	
+	if(item.getAttribute('class') != 'widget list_widget item_wrapper commentable') {
+		continue;
+	}
+	
+	var itemId = item.getAttribute('record');
+	
+	if(itemId == null) {
+		continue;
+	}
+	
+	var span = document.getElementById('item_wrap_' + itemId);
+	
+	span.innerHTML = '[' + itemId + '] ' + span.innerHTML;
+	
+	console.log(item.getAttribute('record'));
+}
